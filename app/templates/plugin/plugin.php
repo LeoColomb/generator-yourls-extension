c530f623917c38453dd674ac37df31a7630ce0d9
Leo Colombaro <git@colombaro.fr>
2014-05-24 16:31:39 +0200

    Move submodules to real files
    Avoid conflict and easier to manage

---
+++ app/templates/plugin/plugin.php
@@ -0,0 +1,22 @@
+<?php
+/*
+Plugin Name: <%= extName %>
+Plugin URI: <%= extUrl %>
+Description: <%= extDesc %>
+Version: <%= extVersion %>
+Author: <%= authorName %>
+Author URI: <%= authorUrl %>
+*/
+
+/*
+
+ Your code goes here.
+ 
+ Suggested read:
+ https://github.com/YOURLS/YOURLS/wiki/Coding-Standards
+ https://github.com/YOURLS/YOURLS/wiki#for-developpers
+ https://github.com/YOURLS/YOURLS/wiki/Plugin-List#get-your-plugin-listed-here
+ 
+ Have fun!
+
+*/
